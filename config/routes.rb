50911f9ad15b6ece4acdf4a8c08f7a4597200a6d
Ruben Pasillas <hobbbes@protonmail.com>
2022-01-31 18:34:05 -0700

    /solarsystem -> /planets

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
 
-  get '/solarsystem', to: 'planets#index'
+  get '/planets', to: 'planets#index'
 
 end
