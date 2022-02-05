c45657ebb3784f409506c52c786a9f71ce030cb2
Jesse Ochs <jessescottochs@gmail.com>
2022-02-05 16:12:01 -0700

    Test: model tests pass

--- spec/models/mountain_range_spec.rb
+++ spec/models/mountain_range_spec.rb
@@ -5,9 +5,4 @@ RSpec.describe MountainRange, type: :model do
         it { should have_many(:mountains) }
     end
 
-    # describe 'validations' do 
-    #     it {  should validate_presence_of(:name) }
-    #     it {  should validate_presence_of(:peak_count) }
-    #     it {  should validate_presence_of(:northern_hemisphere) }
-    # end
 end 
--- spec/models/mountain_spec.rb
+++ spec/models/mountain_spec.rb
@@ -1,13 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Mountain, type: :model do
-    # describe 'relationship' do 
-    #     it { should belong_to(:mountainrange) }
-    # end
+    describe 'relationship' do 
+        it { should belong_to(:mountain_range) }
+    end
 
-    # describe 'validations' do 
-    #     it {  should validate_presence_of(:name) }
-    #     it {  should validate_presence_of(:height) }
-    #     it {  should validate_presence_of(:fourteener) }
-    # end
 end 
