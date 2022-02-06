9da570f8f44677caf9812232b28b254f0c43c0a7
Jesse Ochs <jessescottochs@gmail.com>
2022-02-06 15:02:02 -0700

    user story 7 passes

--- app/models/mountain_range.rb
+++ app/models/mountain_range.rb
@@ -1,3 +1,7 @@
 class MountainRange < ApplicationRecord
     has_many :mountains, dependent: :destroy
+
+    def count_mountains
+        self.mountains.count
+    end
 end
--- spec/features/mountain_ranges/show_spec.rb
+++ spec/features/mountain_ranges/show_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe 'mountain range show' do
     before(:each) do 
         @rockies = MountainRange.create!(name:"Rocky Mountains", peak_count: 77, northern_hemisphere: true)
         @himalayas = MountainRange.create!(name: "Himalayas", peak_count: 110, northern_hemisphere: true)
-        @andes = MountainRange.create!(name: "Andes", peak_count: 100, northern_hemisphere: false)  
+        @andes = MountainRange.create!(name: "Andes", peak_count: 100, northern_hemisphere: false) 
+        
+        
+        @mount_evans = @rockies.mountains.create!(name: 'Mount Evans', height: 14265, fourteener: true)
+        @pikes_peak = @rockies.mountains.create!(name: 'Pikes Peak', height: 14115, fourteener: true)
+        @ypsilon = @rockies.mountains.create!(name: 'Ypsilon', height: 13520, fourteener: false)
     end
 
     scenario 'visitor sees mountain range attributes on show page' do 
@@ -27,5 +32,12 @@ RSpec.describe 'mountain range show' do
         expect(page).to have_content(@andes.name)
         expect(page).to have_content(@andes.peak_count)
         expect(page).to have_content(@andes.northern_hemisphere)
+    end
+
+
+    scenario 'visitor sees count of mountains associated with the range' do
+        visit "/mountainranges/#{@rockies.id}" 
+        expect(page).to have_content("Mountains count: 3")
+  
     end
 end
