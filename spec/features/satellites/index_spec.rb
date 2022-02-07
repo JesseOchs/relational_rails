8c4d222efb755144cc43c4240be13ed2247072f4
Ruben Pasillas <hobbbes@protonmail.com>
2022-02-06 20:31:18 -0700

    update tests

--- spec/features/satellites/index_spec.rb
+++ spec/features/satellites/index_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'Satellites' do
     visit "/satellites"
   end
 
-  it 'lists all planets' do
+  it 'lists all satellites and info' do
     expect(page).to have_content(@satellite.name)
     expect(page).to have_content(@satellite.discovery_year)
     expect(page).to have_content(@satellite.artificial)
@@ -26,9 +26,13 @@ RSpec.describe 'Satellites' do
     expect(page).to have_content(@satellite_two.planet_id)
 
 
-
     expect(page).to_not have_content(@planet.name)
     expect(page).to_not have_content(@planet_two.name)
+  end
+
+  it 'links to respective satellites' do
+    expect(page).to have_link("#{@satellite.name}", :href => "/satellites/#{@satellite.id}")
+    expect(page).to have_link("#{@satellite_two.name}", :href => "/satellites/#{@satellite_two.id}")
   end
 
 end
