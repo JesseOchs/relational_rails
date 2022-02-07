5664b0f0b9e9122c1568ff1f3aad8430bebe59a0
Ruben Pasillas <hobbbes@protonmail.com>
2022-02-06 20:18:40 -0700

    update models

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,4 +1,6 @@
 class Planet < ApplicationRecord
+  has_many :satellites, :dependent => :destroy
+
   validates :name, presence: true
   validates :relative_position, presence: true
   validates :dwarf, inclusion: { in: [true, false] } # May only be boolean values
--- app/models/satellite.rb
+++ app/models/satellite.rb
@@ -1,4 +1,6 @@
 class Satellite < ApplicationRecord
+  belongs_to :planet
+
   validates :name, presence: true
   validates :discovery_year, presence: true
   validates :artificial, inclusion: { in: [true, false] } # May only be boolean values
