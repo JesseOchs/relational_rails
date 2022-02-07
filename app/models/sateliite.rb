11350705a2305a5f7805f98abeae96ede0f7f500
Ruben Pasillas <hobbbes@protonmail.com>
2022-02-06 20:03:38 -0700

    rename

--- app/models/sateliite.rb
+++
@@ -1,5 +0,0 @@
-class Satellite < ApplicationRecord
-  validates :name, presence: true
-  validates :discovery_year, presence: true
-  validates :artificial, inclusion: { in: [true, false] } # May only be boolean values
-end
