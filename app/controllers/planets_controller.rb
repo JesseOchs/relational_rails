4ecc13a457a0fec7dff684b1bdd60fb474fac1ae
Ruben Pasillas <hobbbes@protonmail.com>
2022-02-09 14:24:09 -0700

    magic commit

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,9 +1,36 @@
 class PlanetsController < ApplicationController
   def index
-    @planets = Planet.all
+    @planets = Planet.desc_order
   end
 
   def show
     @planet = Planet.find(params[:id])
+    @satellites = @planet.satellites.all
+  end
+
+  def new
+  end
+
+  def create
+
+    binding.pry
+    planet = Planet.create({
+      name: params[:planet_name],
+      relative_position: params[:relative_position],
+      dwarf: params[:is_dwarf]
+    })
+
+    redirect_to '/planets'
+  end
+
+  def edit
+    @current_planet = Planet.find(params[:id])
+  end
+
+  def update
+    current_planet = Planet.find(params[:id])
+    current_planet.update(planet_params)
+
+    redirect_to "/planets/#{planet.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,36 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
 
+  # Parent
   get '/planets', to: 'planets#index'
+  get '/planets/new', to: 'planets#new'
   get '/planets/:id', to: 'planets#show'
+  get '/planets/:id/edit', to: 'planets#edit'
 
+  post '/planets', to: 'planets#create'
+
+  patch '/planets/:id', to: 'planets#update'
+
+  delete '/planets/:id', to: 'planets#destroy'
+
+  # Child
   get '/satellites', to: 'satellites#index'
   get '/satellites/:id', to: 'satellites#show'
+  get '/satellites/:id/edit', to: 'satellites#edit'
 
+  patch '/satellites/:id', to: 'satellites#update'
+
+  delete '/satellites/:id', to: 'satellites#destroys'
+
+  # Nested Children
   get '/planets/:id/satellites', to: 'planet_satellites#index'
+  get '/planets/:id/satellites/new', to: 'planet_satellites#new'
+  get '/planets/:id/satellites/:satellite_id', to: 'planet_satellites#show'
+  get '/planets/:id/satellites/:satellite_id/edit', to: 'planet_satellites#edit'
+
+  post '/planets/:id/satellites', to: 'planet_satellites#create'
+
+  patch '/planets/:id/satellites/:satellite_id', to: 'planet_satellites#update'
+
+  delete '/planets/:id/satellites/:satellite_id', to: 'planet_satellites#destroy'
 end
