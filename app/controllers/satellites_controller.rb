c4ebebd81ffeb6d581c91e5bd41e12cd577dc91a
Ruben Pasillas <hobbbes@protonmail.com>
2022-02-09 16:44:48 -0700

    satellite updates in direct child directory, complete it 2

--- app/controllers/satellites_controller.rb
+++ app/controllers/satellites_controller.rb
@@ -10,4 +10,26 @@ class SatellitesController < ApplicationController
   def show
     @satellite = Satellite.find(params[:id])
   end
+
+  def edit
+    @current_satellite = Satellite.find(params[:id])
+  end
+
+  def update
+    current_satellite = Satellite.find(params[:id])
+    current_satellite.update(satellite_params)
+
+    redirect_to "/satellites/#{current_satellite.id}"
+  end
+
+  def destroy
+    satellite = Satellite.find(params[:id])
+    satellite.destroy
+    redirect_to "/satellites"
+  end
+
+  private
+  def satellite_params
+    params.permit(:name, :discovery_year, :artificial)
+  end
 end
